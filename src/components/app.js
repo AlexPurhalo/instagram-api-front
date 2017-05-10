ce6c9587a3fd9b7555c74e5f21e35b73ddcbfc7b
AlexPurhalo <alexpurhalo@gmail.com>
2017-05-10 22:50:17 +0300

    instagram-session-code-receiving

--- src/components/app.js
+++ src/components/app.js
@@ -1,9 +1,37 @@
 // Node modules import
-import React, { Component } from 'react'
+import React, {Component} from 'react';
+import { browserHistory } from 'react-router';
+import axios from 'axios'
 
 // Layout component
 export default class App extends Component {
-	render() {
-		return <div className="container">App is working!</div>;
+	componentWillMount() {
+		const query = this.props.location.query,
+			apiUrl 		= 'http://localhost:5000', // back-end url
+			cbAddress = 'auth/instagram'; // call back address
+
+		query.code && axios.get(`${apiUrl}/${cbAddress}/callback?code=${query.code}`)
+			.then(response => console.log(response.data))
+			.then(browserHistory.push('/'))
+	}
+
+	render () {
+		const serviceUrl = 'https://api.instagram.com',
+			clientId = 'b2c21614a6854184af420c92673f46e9',
+			redirectUri = 'http://localhost:8080/',
+			responseType = 'code';
+
+		const authUrl = `
+			${serviceUrl}/oauth/authorize/
+			?client_id=${clientId}
+			&redirect_uri=${redirectUri}
+			&response_type=${responseType}
+		`;
+
+		return (
+			<div className="container">
+				<a href={authUrl}>Auth with Instagram</a>
+			</div>
+		)
 	}
 }
